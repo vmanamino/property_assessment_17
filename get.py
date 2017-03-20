5a0a482215c2b0bf0967c94aeef301d8ce756bf3
vmanamino <vmanamino@gmail.com>
2017-03-20 17:56:08 +0000

    initial get request

---
+++ get.py
@@ -0,0 +1,19 @@
+import json
+import urllib2
+import urllib
+
+def get_data():
+    
+    url = 'https://data.boston.gov/api/action/datastore_search?resource_id=9a4b1173-89ac-4a01-93e7-661eeb81ba16'
+    request = urllib2.Request(url)
+    
+    try:
+        return urllib2.urlopen(request)
+    except urllib2.HTTPError as err:
+        return err
+
+if (get_data().code == 200):
+    
+else:
+    print(get_data().code)
+    

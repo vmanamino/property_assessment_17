ca24b3c51e24074cb349e15ef7adf9828c729e0e
vmanamino <vmanamino@gmail.com>
2017-04-04 03:13:40 +0000

    done

--- data.py
+++ data.py
@@ -7,18 +7,19 @@ output = open('property_assess_17.txt', 'w')
 output.write('Parcel ID\tAddress\tZipcode\tFull address\tOwner\tLand use\tLot size'
 '\tLiving area\tTotal value\tLand value\tBuilding value\tGross tax\n')
 
-# if sys.argv[1]:
-#     total = int(sys.argv[1])
-#     print(total)
-# else:
-#     total = 0
-
-the_set = get.parse_data()
+offset = 0
+total = get.total(offset)
 count = 0
-for s in the_set:
-    count += 1
+while offset < total:
+    the_set = get.parse_data(offset)
+    for s in the_set:
+        count += 1
+        output.write('%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n' % (s.pid, s.address_one.strip()
+        , s.address_zip, s.address.strip(), s.owner, s.land_use, s.lot_size, s.living_area
+        , s.total_value, s.land_value, s.building_value, s.gross_tax ))
+    offset += 100
     print(count)
-    output.write('%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s\n' % (s.pid, s.address_one.lstrip()
-    , s.address_zip, s.address.lstrip(), s.owner, s.land_use, s.lot_size, s.living_area
-    , s.total_value, s.land_value, s.building_value, s.gross_tax ))
+
+
+
     
--- get.py
+++ get.py
@@ -13,18 +13,21 @@ def get_data(offset):
     except urllib2.HTTPError as err:
         return err
 
-def parse_data():
+def parse_data(offset):
     the_set = []
-    offset = 0
+    response = get_data(offset)
+    data = json.loads(response.read())
+    
+    for row in data['result']['records']:
+        parcel_record = Record(row)
+        the_set.append(parcel_record)
+        
+    return the_set
+
+def total(offset):
     response = get_data(offset)
     data = json.loads(response.read())
     total = data['result']['total']
-    while offset < total:
-        response = get_data(offset)
-        data = json.loads(response.read())
-        for row in data['result']['records']:
-            parcel_record = Record(row)
-            the_set.append(parcel_record)
-        offset += 100
-        return the_set
+    return total
+    
     
